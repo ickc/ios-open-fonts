202007fa50a9d1655912430af2673786935a2804
KolenCheung <christian.kolen@gmail.com>
2016-03-31 15:53:38 -0700

    Changed url structure

--- _build-production.sh
+++ _build-production.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-cp README.md index.md
 open http://127.0.0.1:4000/open-fonts-for-ios/
 JEKYLL_ENV=production bundle exec jekyll serve --profile --drafts --incremental
